0bb3d0b19223b43652ac9d3f6654a06b10997eb1
Xander <xj.vreeswijk@student.avans.nl>
2021-12-20 11:42:04 +0100

    updated augmentation.py and added map for original point clouds

--- Augmentation.py
+++ Augmentation.py
@@ -3,48 +3,36 @@ import pandas as pd
 import tensorflow as tf
 import random as rd
 import open3d as o3d
+import os
 
-data = pd.read_csv("usertest1_pc.csv")
+par = 20
+folder = "OriginalPointClouds"
+for number,file in enumerate(os.listdir(folder)):
+    if file.endswith(".csv"):
+        foot = pd.read_csv(os.path.join(folder,file))
 
-points = []
-for i in range(len(data)):
-    points.append([data["X"][i],data["Y"][i],data["Z"][i]])
+        points = []
+        for i in range(len(foot)):
+            points.append([foot["X"][i],foot["Y"][i],foot["Z"][i]])
 
-par = 5
-
-pointsOrg = tf.random.shuffle(points)
-pcd = o3d.geometry.PointCloud()
-pcd.points = o3d.utility.Vector3dVector(pointsOrg)
-o3d.io.write_point_cloud("PointClouds/pc1.ply", pcd)
-#np.savetxt('PointClouds/pc1.csv', pointsOrg, delimiter=',',header='X,Y,Z',comments='') 
-
-pointsMov = tf.random.shuffle(points)
-pointsMov += tf.random.uniform(pointsMov.shape, -par, par, dtype=tf.float64)
-pcd = o3d.geometry.PointCloud()
-pcd.points = o3d.utility.Vector3dVector(pointsMov)
-o3d.io.write_point_cloud("PointClouds/pc2.ply", pcd)
-#np.savetxt('PointClouds/pc2.csv', pointsMov, delimiter=',',header='X,Y,Z',comments='') 
-
-ranges = range(3,21)
-percentOrg = 0.1
-percentMov = 0.1
-for i in ranges:
-    length = len(points)
-    if (i % 2) == 1:
-        new_valOrg = int(percentOrg*length)
-        ExtraOrg = tf.random.shuffle(pointsOrg + tf.random.uniform(pointsOrg.shape, -par, par, dtype=tf.float64))
-        NewPCOrg = tf.concat([pointsOrg, ExtraOrg[:new_valOrg]], axis=0)
-        pcd = o3d.geometry.PointCloud()
-        pcd.points = o3d.utility.Vector3dVector(NewPCOrg)
-        o3d.io.write_point_cloud("PointClouds/pc"+ str(i) +".ply", pcd)
-        #np.savetxt('PointClouds/pc'+str(i)+'.csv', NewPCOrg, delimiter=',',header='X,Y,Z',comments='') 
-        percentOrg += 0.1
-    else:
-        new_valMov = int(percentMov*length)
-        ExtraMov = tf.random.shuffle(pointsMov + tf.random.uniform(pointsMov.shape, -par, par, dtype=tf.float64))
-        NewPCMov = tf.concat([pointsMov, ExtraMov[:new_valMov]], axis=0)
-        pcd = o3d.geometry.PointCloud()
-        pcd.points = o3d.utility.Vector3dVector(NewPCMov)
-        o3d.io.write_point_cloud("PointClouds/pc"+ str(i) +".ply", pcd)
-        #np.savetxt('PointClouds/pc'+str(i)+'.csv', NewPCMov, delimiter=',',header='X,Y,Z',comments='') 
-        percentMov += 0.1
+        for movement in np.arange(0,0.75,0.15):
+            for adding in np.arange(0,1,0.2):
+                length = len(points)
+                if movement != 0:
+                    NewPoints = tf.random.shuffle(points)
+                    lengthNew = int(movement * len(NewPoints))
+                    NewPointsLeft = NewPoints[:lengthNew]
+                    NewPointsRight = NewPoints[lengthNew:]
+                    NewPointsLeft += tf.random.uniform(NewPointsLeft.shape, -par, par, dtype=tf.float64)
+                    NewPoints = tf.concat([NewPointsRight, NewPointsLeft], axis=0)
+                    NewPoints = tf.random.shuffle(NewPoints)
+                else:
+                    NewPoints = tf.random.shuffle(points)
+                if adding != 0:
+                    length = len(NewPoints)
+                    new_valOrg = int(adding*length)
+                    ExtraPoints = tf.random.shuffle(NewPoints + tf.random.uniform(NewPoints.shape, -par, par, dtype=tf.float64))
+                    NewPoints = tf.concat([NewPoints, ExtraPoints[:new_valOrg]], axis=0)
+                pcd = o3d.geometry.PointCloud()
+                pcd.points = o3d.utility.Vector3dVector(NewPoints)
+                o3d.io.write_point_cloud("PointClouds/pointcloud{}_{:.2f}_{:.2f}.ply".format(number,movement,adding), pcd)
