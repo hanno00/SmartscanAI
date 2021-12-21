d67b6a3d9d1510967ab2e823af0cde64a98e3a7c
Hanno Brandwijk <brandwijkh@gmail.com>
2021-12-21 11:36:01 +0100

    Add Cost Function

--- Triangulation.py
+++ Triangulation.py
@@ -10,23 +10,26 @@ class Triangulation:
     def convert_pointcloud_to_faces(self, path):
         print("Load a ply point cloud, print it, and render it")
         pcd = self.load_pcd(path)
-        # self.draw_pcd(pcd, "Original Pointcloud")
+        self.draw_pcd(pcd, "Original Pointcloud")
     
         print("Downsample the point cloud with a voxel of 0.05")
         pcd = self.voxel_down_sample(pcd,12)
-        # self.draw_pcd(pcd, "Downsampled")
+        self.draw_pcd(pcd, "Downsampled")
 
-        print("Recompute the normal of the downsampled point cloud")
+        print("Compute the normal of the downsampled point cloud")
         pcd = self.computing_normals(pcd,20,5)
         # self.draw_pcd(pcd, "With computed normals")
-
-        self.print_points(pcd,5)    
-        self.print_normals(pcd,5)
+        # pcd = self.orient_normals(pcd,10)
+        # self.compute_cost(pcd,5)
 
         print("Orient normals")
-        for i in range(5):
+        for i in range(10):
             pcd = self.orient_normals(pcd,(i + 1) * 5)
-            self.drawPcd(pcd, "Oriented Normals")
+            start = time.time()
+            print("angle: ", self.compute_cost(pcd,5))
+            end = time.time()
+            print("time: ", end - start)
+            # self.drawPcd(pcd, "Oriented Normals")
 
     def voxel_down_sample(self, pcd, size):
         return pcd.voxel_down_sample(voxel_size=size)
@@ -68,11 +71,42 @@ class Triangulation:
     def draw_pcd(self, pcd, title):
         o3d.visualization.draw_geometries([pcd],title,mesh_show_back_face=True)
 
-    def print_points(self, pcd, x):
-        print("Print first X points")
-        print(np.asarray(pcd.points)[:x])
+    def get_points(self, pcd, x):
+        print("Get first X points")
+        return np.asarray(pcd.points)[:x]
     
-    def print_normals(self, pcd, x):
-        print("Print first X normals")
-        print(np.asarray(pcd.normals)[:x])
+    def get_normals(self, pcd, x):
+        print("Get first X normals")
+        return np.asarray(pcd.normals)[:x]
     
+    def compute_cost(self,pcd,knn):
+        pcd_tree = o3d.geometry.KDTreeFlann(pcd)
+        amount_of_points = len(np.asarray(pcd.points))
+        total_angle = 0
+        for i in range(amount_of_points):
+            total_angle += self.get_total_angle_from_index(pcd,pcd_tree,knn,i)
+
+        return total_angle / amount_of_points / knn
+
+    def get_total_angle_from_index(self,pcd,pcd_tree,knn,i):
+        
+        normals = self.get_normals_from_neighbours(pcd,pcd_tree,knn,i)
+        anchor_vector = normals[0].tolist()
+        
+        unit_anchor_vector = anchor_vector / np.linalg.norm(anchor_vector)
+        angles = 0
+        for j in range(1,knn):
+            n_vector = normals[j].tolist()
+            unit_n_vector = n_vector / np.linalg.norm(n_vector)
+            dot_product = np.dot(unit_anchor_vector, unit_n_vector)
+            angle = np.arccos(dot_product)
+
+            # Check for isnan when two vectors are parallel
+            if (not np.isnan(angle)):
+                angles += angle
+
+        return angles
+    
+    def get_normals_from_neighbours(self,pcd,pcd_tree,knn,i):
+        [k, idx, _] = pcd_tree.search_knn_vector_3d(pcd.points[i], knn)
+        return np.asarray(pcd.normals)[idx]
